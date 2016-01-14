f2f666c40712dc3a55ef9655c2b14258548d0fe0
Casey Kim <k.yesac@gmail.com>
2016-01-14 15:48:02 -0800

    create mvc for users

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :user do
     name { FFaker::Name.name }
+
+    factory :user_with_videos do
+      after(:create) do |user|
+        3.times { create(:users_video, user: user) }
+      end
+    end
   end
 end
