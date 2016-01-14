c5ef7361771f7b1cb2e8e9c3b75293570288ddb4
Casey Kim <k.yesac@gmail.com>
2016-01-14 14:56:05 -0800

    create models, controllers

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,9 @@
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+  end
+
+  def show
+    @videos = Video.find(params[:id])
+  end
 end
--- app/models/users_video.rb
+++ app/models/users_video.rb
@@ -1,2 +1,7 @@
 class UsersVideo < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :video
+
+  validates :user, presence: true
+  validates :video, presence: true
 end
